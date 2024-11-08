41f2d071860332bfdff7a160782c6c1a2349088c
fefe0107 <161839327+fefe0107@users.noreply.github.com>
2024-11-08 09:34:31 -0300

    Add files via upload

---
+++ flashcard.2c-main/flashcard-aula_10/perguntas.js
@@ -0,0 +1,33 @@
+criaCartao(
+    'Fundo do mar',
+    'Qual o lugar mais profundo do oceano?',
+    'O lugar mais profundo é a Fossa das Marianas com -10.994 metros '
+)
+
+criaCartao(
+    'Fundo do mar ',
+    'Qual é o maior oceano do mundo?',
+    'Oceano Pacífico'
+)
+
+criaCartao(
+    'Fundo do mar ',
+    'Qual é o maior vulcão submarino do mundo?',
+    'Tamu Massif ocalizao no oceano pacífico'
+)
+
+criaCartao(
+    'Fundo do mar ',
+    'Qual espécie e medusa mais perigosa?',
+    'Cubomedusa'
+)
+criaCartao(
+    'Fundo do mar ',
+    'qual a maior espécie aquática?',
+    'Baleia azul'
+)
+criaCartao(
+    'Fundo do mar ',
+    'qual foi a maior profundidade mergulhada?',
+    '-10.928 metros'
+)
